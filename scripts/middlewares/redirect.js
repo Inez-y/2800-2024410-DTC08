9b067b3f72aea91aa44a3b50b6bab9819ff5743b
Daylen Smith <daylencsmith@gmail.com>
2024-05-14 15:41:32 -0700

    Add scripts folder and modularize code

---
+++ scripts/middlewares/redirect.js
@@ -0,0 +1,20 @@
+/**
+ * Middleware that will redirect the user
+ */
+
+/**
+ * Redirect to login page if not logged in
+ * @param {*} req request
+ * @param {*} res response
+ * @param {*} next next
+ * @returns None
+ */
+const redirectIfNotLoggedIn = (req, res, next) => {
+    if (req.session.loggedin) {
+        return next();
+    } else {
+        res.redirect('/');
+    }
+}
+
+export default redirectIfNotLoggedIn;

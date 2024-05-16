789185c71697ecdc8b8a307d4b4325e638a70d52
Inez Yoon <inez3cs@gmail.com>
2024-05-15 19:38:45 -0700

    Change footer and header design

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 // Connect to MongoDB
-mongoose.connect(`mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/`, 
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true, 
-    }).then(() => {
-        console.log('Connected to MongoDB');
-    }).catch((err) => {
-        console.log('Failed to connect to MongoDB', err);
-    }
-);
+mongoose.connect(`mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Failed to connect to MongoDB', err);
+});
 
 const store = new mongoDBSession({
     uri: process.env.MONGODB_URI,
@@ -88,7 +88,10 @@ app.use('/', authPages);
 
 
 app.get("*", (req, res) => {
-    res.status(404).render("error", {msg: "Page not found", code: 404});
+    res.status(404).render("error", {
+        msg: "Page not found",
+        code: 404
+    });
 });
 
 // Start the server

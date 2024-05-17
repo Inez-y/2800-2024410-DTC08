86da4a6e59c20856696b14caa47b24002874fd71
Alice Huang <alice.in.realityland@gmail.com>
2024-05-16 21:26:14 -0700

    Add basic handling of openAI API from landing page and improve landing page css to work with API

--- scripts/routes/no_authentication.js
+++ scripts/routes/no_authentication.js
@@ -5,12 +5,33 @@
 const express = require('express');
 const router = express.Router();
 
+const { generateRecipe, validateQuery, generateRecipeFromKitchen } = require('../middlewares/openAI_controller');
+
 /**
  * Renders the landing page
  * @author Daylen Smith
  */
 router.get('/', async (req, res) => {
-    res.render('landing');
+    res.render('landing', {response: null, query: null, show: true});
+});
+
+/**
+ * Generates a recipe based on the user's query and renders the landing page with the response
+ * @author Alice Huang
+ */
+router.post('/', async (req, res) => {
+    let query = req.body.query;
+    let valid = await validateQuery(query);
+    console.log(valid);
+    if (valid === 'recipe') {
+        let response = await generateRecipe(query, [{role: 'system', content: 'You are a helpful assistant. You generate recipes based on user queries.'}]);
+        console.log(response);
+        res.render('landing', {response: response, query: query, show: null});
+    } else if (valid === 'kitchen') {
+        res.render('landing', {response: 'You have ... in your kitchen', query: query, show: null});
+    } else {
+        res.render('landing', {response: 'That is not a valid query. Please try again.', query: query, show: null});
+    }
 });
 
 /**

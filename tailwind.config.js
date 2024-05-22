9ef3687d91683c2470cb3e4fb87dbc63709e38cd
tonylai2022 <mingwalai326@gmail.com>
2024-05-21 21:18:17 -0700

    Test tailwind on html

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ["./views/**/*.{html,js,ejs}", "./test.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Poppins': ['"Poppins"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'buttongreen': '#779341',
+      },
+    },
+  },
+  plugins: [],
+};

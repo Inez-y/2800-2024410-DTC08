1444cde2b250a957c6880843e6bb9ef3acdc345a
Inez Yoon <inez3cs@gmail.com>
2024-05-22 11:50:02 -0700

    Update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,6 @@ module.exports = {
         'buttongreen': '#779341',
         'lightGrey': '#9A9BB1',
       },
-      backgroundImage: {
-        'backgroundImage': "url('/public/background1.jpeg')",
-      }
     },
   },
   plugins: [],
